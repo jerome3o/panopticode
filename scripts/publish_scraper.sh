2f8f6b357c1059bc0c195a15d386aca0aa18f3f9
Jerome Swannack <jeromeswannack@gmail.com>
2021-10-09 12:36:13 +1300

    fixed up scaper dockerfile for not arm

--- scripts/publish_scraper.sh
+++ scripts/publish_scraper.sh
@@ -3,7 +3,7 @@ cd scraper
 NAME=bankscraper
 TAGS=0.0.1
 
-docker buildx build --platform=linux/arm64 . -t jerome3o/$NAME:$TAGS-arm
-docker push jerome3o/$NAME:$TAGS-arm
+docker build . -t jerome3o/$NAME:$TAGS
+docker push jerome3o/$NAME:$TAGS
 
 cd .. 
