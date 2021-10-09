a8b8a14d7b6017dde7e9c2df7b27042ba0a89135
Jerome Swannack <jeromeswannack@gmail.com>
2021-10-09 13:31:41 +1300

    fixing up server

--- ingress/apitools/service.py
+++ ingress/apitools/service.py
@@ -10,26 +10,26 @@ def configure_api(
     endpoint_base: str,
 ) -> None:
     @app.get(endpoint_base, response_model=List[dto_class])
-    def get_all(self) -> List[dto_class]:
+    def get_all() -> List[dto_class]:
         return api.get_all()
 
     @app.get(endpoint_base + "/{item_id}", response_model=dto_class)
-    def get(self, item_id: str) -> dto_class:
+    def get(item_id: str) -> dto_class:
         return api.get()
 
     @app.post(endpoint_base, response_model=dto_class)
-    def create(self, item: dto_class) -> dto_class:
+    def create(item: dto_class) -> dto_class:
         return api.create()
 
     @app.put(endpoint_base, response_model=dto_class)
-    def idempotent_create(self, item: dto_class) -> dto_class:
+    def idempotent_create(item: dto_class) -> dto_class:
         return api.idempotent_create()
 
     @app.post(endpoint_base + "/{item_id}", response_model=dto_class)
-    def update(self, item_id: str, item: dto_class) -> dto_class:
+    def update(item_id: str, item: dto_class) -> dto_class:
         return api.update()
 
     @app.delete(endpoint_base + "/{item_id}", response_model=int)
-    def delete(self, item_id: str) -> int:
+    def delete(item_id: str) -> int:
         return api.delete()
 
