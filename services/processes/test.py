3ab8f1bdfdbf52f6f2f8948bb55e80f98bbac927
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-17 15:19:38 +1200

    Added test script

---
+++ services/processes/test.py
@@ -0,0 +1,19 @@
+import os
+from requests import get
+
+_key = os.environ["API_KEYS"].split(",")[0]
+
+
+def main():
+    resp = get(
+        "http://localhost:8000/ps",
+        headers={"X-API-Key": _key},
+    )
+    print(resp.json())
+
+
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    main()
