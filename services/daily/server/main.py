737870bc82378943674eef0296a1d9e9ab667742
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-18 17:56:18 +1200

    Added get_today_record endpoint

--- services/daily/server/main.py
+++ services/daily/server/main.py
@@ -8,6 +8,8 @@ from server.models import DailySelfReportStorage, DailySelfReportTransfer
 
 app = FastAPI()
 
+# TODO(j.swannack): api keys
+
 
 # todo dependency inject db client
 @app.on_event("startup")
@@ -28,3 +30,15 @@ async def create_record(record: DailySelfReportTransfer):
     result = await app.mongodb['daily_self_report'].insert_one(storage_record.model_dump())
     storage_record.id = str(result.inserted_id)
     return storage_record
+
+
+# Endpoint to check if there has been a report today
+@app.get("/records/today/", response_model=DailySelfReportStorage)
+async def get_today_record():
+    today = datetime.now().date()
+    result = await app.mongodb['daily_self_report'].find_one({'created_timestamp': {'$gte': today}})
+    if result:
+        return DailySelfReportStorage.model_validate(result)
+    else:
+        # 404
+        return None
