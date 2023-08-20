00c82d44ce6845979078b109e7e498c7258b49c4
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-20 16:02:06 +1200

    Updating frontend to use new url

--- services/daily/frontend/publish.sh
+++ services/daily/frontend/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get first arg as URL, if none then use default
-URL=${1:-http://localhost:8000/}
+URL=${1:-http://daily-be.k8s.jeromeswannack.com/}
 
 # build
 sudo docker build --build-arg URL=$URL -t jerome3o/daily-frontend .
