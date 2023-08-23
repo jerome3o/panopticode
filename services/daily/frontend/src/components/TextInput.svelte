3ee215a9f35f8b55f90f259ac4de7da3b8d41173
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-23 21:19:21 +1200

    Added test input component

---
+++ services/daily/frontend/src/components/TextInput.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let value: string = "";
+    export let title: string = "Text Input";
+</script>
+
+<label for="textinput">{title}</label>
+<textarea id="textinput" bind:value={value}></textarea>
