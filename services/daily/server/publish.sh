206812837051b687254af3b7294d42805d96f8ea
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-24 16:53:11 +1200

    Removed erroneous build arg from daily server

--- services/daily/server/publish.sh
+++ services/daily/server/publish.sh
@@ -3,7 +3,7 @@
 # may be required: sudo docker login
 
 # build
-sudo docker build --build-arg URL=$URL -t jerome3o/daily-server .
+sudo docker build -t jerome3o/daily-server .
 
 # publish
 sudo docker push jerome3o/daily-server
