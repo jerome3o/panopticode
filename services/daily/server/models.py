730c2c26757893441944b3b01b62d6edd7d0fd6d
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-24 13:38:47 +1200

    Added bson ObjectId validation to models

--- services/daily/server/models.py
+++ services/daily/server/models.py
@@ -1,7 +1,18 @@
-from typing import Optional
+from typing import Optional, Union, Annotated
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from pydantic.functional_validators import BeforeValidator
+from bson import ObjectId
+
+
+def bson_id_to_str(v: Optional[Union[str, ObjectId]]) -> Optional[str]:
+    if v is not None:
+        return str(v)
+    return v
+
+
+BsonId = Annotated[Optional[str], BeforeValidator(bson_id_to_str)]
 
 
 class DailySelfReport(BaseModel):
@@ -19,9 +30,12 @@ class DailySelfReportTransfer(BaseModel):
 
 # Data Storage Object
 class DailySelfReportStorage(DailySelfReportTransfer):
-    id: Optional[str] = None
+    id: BsonId = Field(None, alias="_id")
     modified_timestamp: Optional[datetime] = None
     created_timestamp: Optional[datetime] = None
+
+    class Config:
+        populate_by_name = True
 
 
 class TodayResponse(BaseModel):
