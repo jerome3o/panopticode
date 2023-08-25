eb770a84623c0c6bd1c1d9674b60518bb2e1b94d
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-25 13:11:40 +1200

    Added constants from previous project

--- services/fitrelay/src/auth.py
+++ services/fitrelay/src/auth.py
@@ -1,24 +1,52 @@
 """
 Handles user Fitbit authentication and receiving OAuth token.
 """
+import secrets
+import hashlib
+import base64
+from urllib.parse import quote
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Request, Response
+from fastapi.responses import RedirectResponse
+
 from db import insert_token, get_token
 from models import Token
+from constants import (
+    AUTH_URL,
+    CLIENT_ID,
+    REDIRECT_URL,
+    STATE,
+    SCOPES,
+)
 
 router = APIRouter()
 
 
-@router.get("/fitbit/authorize")
-def authorize():
-    # Make an authorization request
-    # Extract access token and refresh token
-    # Call `insert_token` to store the tokens in DB
-    pass
+@router.get("/login")
+def login(request: Request) -> Response:
+    """Redirect the user to the Fitbit authorization page."""
+    # Generate the code verifier and code challenge
+    code_verifier = secrets.token_hex(32)
+    code_challenge = (
+        base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode()).digest())
+        .decode()
+        .rstrip("=")
+    )
 
+    # Save the code verifier in the user's session
+    request.session["code_verifier"] = code_verifier
 
-@router.get("/fitbit/refresh")
-def refresh():
-    # Make a refresh token request
-    # Update the tokens in DB
-    pass
+    # Construct the authorization URL
+    auth_url = (
+        f"{AUTH_URL}?"
+        "response_type=code"
+        f"&client_id={CLIENT_ID}"
+        f"&redirect_uri={quote(REDIRECT_URL)}"
+        f"&state={STATE}"
+        f"&code_challenge={code_challenge}"
+        f"&scope={'%20'.join(SCOPES)}"
+        "&code_challenge_method=S256"
+    )
+
+    # Redirect the user to the authorization URL
+    return RedirectResponse(auth_url)
---
+++ services/fitrelay/src/constants.py
@@ -0,0 +1,51 @@
+import os
+import secrets
+
+
+# Env vars
+# URL
+# OAUTH_ID
+# OAUTH_SECRET
+# STATE_SECRET
+
+URL = os.environ.get("URL", "http://localhost:8000").rstrip("/")
+CLIENT_ID = os.environ.get("OAUTH_ID")
+CLIENT_SECRET = os.environ.get("OAUTH_SECRET")
+# A state value to use for CSRF protection
+STATE = os.environ.get("STATE_SECRET") or secrets.token_hex(16)
+
+
+# The authorization URL for Fitbit's OAuth 2.0 authorization server
+AUTH_URL = "https://www.fitbit.com/oauth2/authorize"
+
+# The token URL for Fitbit's OAuth 2.0 authorization server
+TOKEN_URL = "https://api.fitbit.com/oauth2/token"
+
+# The URL of your application
+REDIRECT_URL = f"{URL}/callback"
+
+# The URL of the protected resource that you want to access
+RESOURCE_URL = (
+    "https://api.fitbit.com/1/user/-/activities/heart/date/2022-12-24/1d.json"
+)
+
+
+# Secret value for session
+SESSION_MIDDLEWARE_SECRET = secrets.token_hex(16)
+
+
+# SCOPE
+SCOPES = [
+    "activity",
+    "heartrate",
+    "location",
+    "nutrition",
+    "oxygen_saturation",
+    "profile",
+    "respiratory_rate",
+    "settings",
+    "sleep",
+    "social",
+    "temperature",
+    "weight",
+]
