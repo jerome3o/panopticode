684da428ef7e90c12254eaaeb63f5f989b71e9e6
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-02 11:33:07 +1200

    Fixed docker call in publish script

--- services/daily/frontend/docker/publish.sh
+++ services/daily/frontend/docker/publish.sh
@@ -2,6 +2,6 @@ SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
 (
     cd $SCRIPT_DIR/..
-    docker build -t jerome3o/daily-frontend .
+    docker build -t jerome3o/daily-frontend -f docker/Dockerfile .
     docker push jerome3o/daily-frontend
 )
